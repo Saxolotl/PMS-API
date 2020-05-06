a7fbf1ede1ecff2541a7a6641179036ed274501e
Saxolotl <erin@saxolotl.xyz>
2020-05-06 14:17:41 +0100

    Added Retrieving of Meetings and Deadlines for Projects

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/login", (req, res) => {
             email: req.query.username,
             password: req.query.password,
         },
-    }).then((result) => {
+    }).then(result => {
         if (result == null) {
             res.sendStatus(400);
         } else {
@@ -40,10 +40,10 @@ app.post("/register", (req, res) => {
         email: req.query.email,
         password: req.query.password,
     })
-        .then((result) => {
+        .then(result => {
             res.status(200).send(`Successfully Registered User`);
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
             res.status(418).send(
                 `An account with that user already exists: ${err}`
@@ -56,9 +56,9 @@ app.post("/createProject", (req, res) => {
         name: req.query.name,
         description: req.query.desc,
         budget: req.query.budget
-    }).then((result) => {
+    }).then(result => {
         res.status(200).send(`Successfully created Project ${result.id}: ${result.name}`);
-    }).catch((err) => {
+    }).catch(err => {
         res.status(500).send(err);
     })
 });
@@ -71,10 +71,44 @@ app.post("/addDeadline", (req, res) => {
         ProjectId: req.query.project
     }).then((result) => {
         res.status(200).send(`Successfully created Deadline ${result.name} for Project: ${result.ProjectId}`);
-    }).catch((err) => {
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.post("/getDeadlinesForProject", (req, res) => {
+    Deadlines.findAll({
+        where: {ProjectId: req.query.projectId}
+    }).then(result => {
+        res.status(200).send(result);
+    }).catch(err => {
         res.status(500).send(err);
     })
+})
+
+app.post("/addMeeting", (req, res) => {
+    Meetings.create({
+        date: req.query.date,
+        name: req.query.name,
+        description: req.query.desc,
+        ProjectId: req.query.project
+    }).then(result => {
+        res.status(200).send(`Successfully created Meeting ${result.name} for Project: ${result.ProjectId}`);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
 });
+
+app.post("/getMeetingsForProject", (req, res) => {
+    Meetings.findAll({
+        where: {ProjectId: req.query.projectId}
+    }).then(result => {
+        res.status(200).send(result);
+    }).catch(err => {
+        res.status(500).send(err);
+    })
+})
+
 
 app.get("/updateDB", (req, res) => {1
     seed();
